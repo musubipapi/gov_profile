177798f482596b0a8bdcb5f63c8c1ebea11d478e
Andy <musubi@andys-mbp.myfiosgateway.com>
2021-11-08 12:47:09 -0500

    fix typing and naming of package

--- common/web3.ts
+++ common/web3.ts
@@ -1,8 +1,14 @@
-import { InjectedConnector } from '@web3-react/injected-connector'
-import { ethers } from 'ethers'
+import { InjectedConnector } from "@web3-react/injected-connector";
+import { ethers } from "ethers";
 
-export const injected = new InjectedConnector({ supportedChainIds: [1, 3, 4, 5, 42] })
+export const injected = new InjectedConnector({
+  supportedChainIds: [1, 3, 4, 5, 42],
+});
 
-export const getLibrary = (provider: ethers.providers.AsyncSendable) => {
-    return new ethers.providers.Web3Provider(provider)
-}
+export const getLibrary = (
+  provider:
+    | ethers.providers.ExternalProvider
+    | ethers.providers.JsonRpcFetchFunc
+) => {
+  return new ethers.providers.Web3Provider(provider);
+};
